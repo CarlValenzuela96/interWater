831d97383b4cebc6d18aab729f3ee2c421abef16
Carl <c.valenzuela06@ufromail.cl>
2018-12-09 03:26:12 -0300

    Paleta de colores y mejora de vistas

--- app/src/main/java/com/interwater/nigaca/interwater/AgregarComunidades.java
+++ app/src/main/java/com/interwater/nigaca/interwater/AgregarComunidades.java
@@ -1,17 +1,46 @@
 package com.interwater.nigaca.interwater;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AgregarComunidades extends Fragment {
+
+    Button infoButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+
         View view = inflater.inflate(R.layout.agregar_comunidades, container, false);
+
+
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        this.infoButton = getActivity().findViewById(R.id.info_search_button);
+
+        this.infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getActivity().getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
     }
 }
