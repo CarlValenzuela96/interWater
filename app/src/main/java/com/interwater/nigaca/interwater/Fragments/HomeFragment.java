f12bfb59b20b04823ba3775559d56bf1f485baa7
Carl <c.valenzuela06@ufromail.cl>
2018-12-13 22:50:31 -0300

    Cambios visuales (comenzando agregado de suministros)

--- app/src/main/java/com/interwater/nigaca/interwater/Fragments/HomeFragment.java
+++ app/src/main/java/com/interwater/nigaca/interwater/Fragments/HomeFragment.java
@@ -7,15 +7,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.interwater.nigaca.interwater.Database.DatabaseHelper;
+import com.interwater.nigaca.interwater.Models.Fecha;
 import com.interwater.nigaca.interwater.R;
 
 public class HomeFragment extends Fragment {
+
+    TextView home_fecha;
 
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.home, container, false);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        this.home_fecha = getActivity().findViewById(R.id.home_fecha);
+
+        DatabaseHelper dbh = new DatabaseHelper(getActivity().getApplicationContext());
+
+        Fecha f = dbh.getFechaActual();
+        this.home_fecha.setText(f.toString());
+
+
     }
 }
